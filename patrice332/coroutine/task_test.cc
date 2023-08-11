991faa0ee3c12109bcf44f54948bfdb144792341
Patrice Laberge <patrice332@gmail.com>
2023-08-11 08:01:35 -0700

    Refactor test

--- patrice332/coroutine/task_test.cc
+++ patrice332/coroutine/task_test.cc
@@ -6,8 +6,30 @@ namespace patrice332::coroutine {
 
 namespace {
 
+constexpr std::size_t kPageSize = 4096;
+
+struct TestStack {
+  TestStack(void* memory, Stack&& new_stack) : Mem{memory}, Stack{new_stack} {}
+  ~TestStack() {
+    if (Mem != nullptr) {
+      free(Mem);
+      Mem = nullptr;
+    }
+  }
+
+  void* Mem;
+  Stack Stack;
+};
+
+TestStack MakeTestStack(std::size_t mem_size) {
+  auto* mem =
+      reinterpret_cast<std::byte*>(std::aligned_alloc(kPageSize, mem_size));
+
+  auto stack = MakeStack(std::span<std::byte>{mem, mem_size});
+  return TestStack{mem, std::move(stack).value()};
+}
+
 TEST(Task, Sanity) {
-  constexpr std::size_t kPageSize = 4096;
   constexpr std::size_t kMemSize = 2 * kPageSize;
   auto* mem =
       reinterpret_cast<std::byte*>(std::aligned_alloc(kPageSize, kMemSize));
@@ -22,33 +44,25 @@ TEST(Task, Sanity) {
 }
 
 TEST(Task, Called) {
-  constexpr std::size_t kPageSize = 4096;
   constexpr std::size_t kMemSize = 2 * kPageSize;
-  auto* mem =
-      reinterpret_cast<std::byte*>(std::aligned_alloc(kPageSize, kMemSize));
-  {
-    auto stack = MakeStack(std::span<std::byte>{mem, kMemSize});
+  auto stack = MakeTestStack(kMemSize);
 
-    ASSERT_TRUE(stack.ok());
+  Task task{stack.Stack};
 
-    Task task{stack.value()};
+  bool called = false;
 
-    bool called = false;
-
-    if (auto status = task.Init(
-            [](void* ptr) { *reinterpret_cast<bool*>(ptr) = true; }, &called);
-        !status.ok()) {
-      ASSERT_TRUE(status.ok());
-    }
-
-    EXPECT_FALSE(called);
-    if (auto status = task.Resume(); !status.ok()) {
-      ASSERT_TRUE(status.ok());
-    }
-
-    EXPECT_TRUE(called);
+  if (auto status = task.Init(
+          [](void* ptr) { *reinterpret_cast<bool*>(ptr) = true; }, &called);
+      !status.ok()) {
+    ASSERT_TRUE(status.ok());
   }
-  free(mem);
+
+  EXPECT_FALSE(called);
+  if (auto status = task.Resume(); !status.ok()) {
+    ASSERT_TRUE(status.ok());
+  }
+
+  EXPECT_TRUE(called);
 }
 
 }  // namespace
